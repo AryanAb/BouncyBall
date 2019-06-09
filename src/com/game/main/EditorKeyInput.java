f48cc3ecdec9d34ceaff25e8dabb30ec1bd73982
AryanAb <47926823+AryanAb@users.noreply.github.com>
2019-06-09 16:29:55 -0400

    Update EditorKeyInput.java

--- src/com/game/main/EditorKeyInput.java
+++ src/com/game/main/EditorKeyInput.java
@@ -1,54 +1,53 @@
 package com.game.main;
 
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.nio.file.StandardOpenOption;
 
-public class EditorKeyInput extends KeyAdapter {
+public class EditorInput extends MouseAdapter {
 
-    private EditorInput eInput;
-    private TextHandler tHandler;
-    private EditorHandler eHandler;
     private LevelEditor editor;
+    private EditorHandler eHandler;
+    private Handler handler;
 
-    public EditorKeyInput(EditorInput eInput, EditorHandler eHandler, LevelEditor editor) {
-        this.eInput = eInput;
-        this.eHandler = eHandler;
+    public boolean BounceTileSelected = true;
+    public boolean DeathTileSelected = false;
+    public boolean StarSelected = false;
+    public boolean PlayerSpawnSelected = false;
+    public boolean HBoostSelected = false;
+    public boolean VBoostSelected = false;
+
+    public EditorInput(LevelEditor editor, EditorHandler eHandler, Handler handler){
         this.editor = editor;
+        this.eHandler = eHandler;
+        this.handler = handler;
     }
 
-    public void keyPressed(KeyEvent e){
-        int key = e.getKeyCode();
+    public int gridFloor(int a){
+        return a - (a % 75);
+    }
 
-        if((key == KeyEvent.VK_S) && ((e.getModifiersEx() & KeyEvent.CTRL_DOWN_MASK) != 0)){
-            tHandler = new TextHandler(eHandler);
+    public void mousePressed(MouseEvent e){
+        int mx = e.getX();
+        int my = e.getY();
 
-            tHandler.save(editor.name, editor.path);
+        Graphics g = null;
 
+        if(BounceTileSelected) {
+            eHandler.addObject(new Tile(gridFloor(mx), gridFloor(my), ID.BounceTile));
+        } else if(DeathTileSelected) {
+            eHandler.addObject(new DeathTile(gridFloor(mx), gridFloor(my), ID.DeathTile));
+        } else if(StarSelected) {
+            eHandler.addObject(new Star(gridFloor(mx) + 25, gridFloor(my) + 30, ID.Star));
+        } else if(PlayerSpawnSelected) {
+            eHandler.addObject(new Player(gridFloor(mx) + 20, gridFloor(my) + 10, ID.Player, handler));
+        } else if(HBoostSelected){
+          eHandler.addObject(new HBoost(gridFloor(mx), gridFloor(my), ID.HBoost));
+        } else if(VBoostSelected){
+          eHandler.addObject(new VBoost(gridFloor(mx), gridFloor(my), ID.VBoost));
         }
-
-        if(key == KeyEvent.VK_1){
-            eInput.BounceTileSelected = true;
-            eInput.DeathTileSelected = false;
-            eInput.StarSelected = false;
-            eInput.PlayerSpawnSelected = false;
-        } else if(key == KeyEvent.VK_2){
-            eInput.DeathTileSelected = true;
-            eInput.BounceTileSelected = false;
-            eInput.StarSelected = false;
-            eInput.PlayerSpawnSelected = false;
-        } else if(key == KeyEvent.VK_3){
-            eInput.StarSelected = true;
-            eInput.DeathTileSelected = false;
-            eInput.BounceTileSelected = false;
-            eInput.PlayerSpawnSelected = false;
-        } else if(key == KeyEvent.VK_4){
-            eInput.BounceTileSelected = false;
-            eInput.DeathTileSelected = false;
-            eInput.StarSelected = false;
-            eInput.PlayerSpawnSelected = true;
-        } else if(key == KeyEvent.VK_5){
-
-        }
+          
 
     }
 
