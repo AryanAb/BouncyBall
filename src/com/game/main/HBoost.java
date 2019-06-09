a5ec99d28ec23fb866bbb3bb437bcbac371e2f4b
AryanAb <47926823+AryanAb@users.noreply.github.com>
2019-06-09 16:33:08 -0400

    Update HBoost.java

--- src/com/game/main/HBoost.java
+++ src/com/game/main/HBoost.java
@@ -1,6 +1,10 @@
 package com.game.main;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class HBoost extends GameObject {
 
@@ -20,12 +24,21 @@ public class HBoost extends GameObject {
 
     }
 
+ 
     @Override
     public void render(Graphics g) {
-        g.setColor(Color.CYAN);
-        g.fillRect(x, y, 75, 75);
-    }
 
+        File path = new File("E:/BouncyBall-master/Assets/HorizontalBooster.png");
+
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(path);
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+
+        g.drawImage(img, x, y, null);
+    }
     public void setDirection(int direction) {
         // depending on the rotation of the image set direction to 1 or -1
     }
